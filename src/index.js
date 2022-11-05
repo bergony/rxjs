1cf880b0b4d1532ed3385ff85ee66267bda80130
bergony.bandeira <bergony.bandeira@quark.tec.br>
2022-11-05 07:35:55 -0300

    [bergony] Obervres Operadors e eventos.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { of, from  } from "rxjs";
+
+const observable = from(
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+)
+
+const subscription = observable.subscribe({
+    next(value){
+        console.log(value)
+    },
+    complete() {
+        console.log('complete');
+    }
+});
+
+console.log('hello');
